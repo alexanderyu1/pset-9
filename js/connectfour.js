0a7dba96e66ab15884f98492be39ac8873b34fd0
Alexander Yu <ayu1@ucvts.org>
2020-02-24 12:39:21 -0500

    30

--- js/connectfour.js
+++ js/connectfour.js
@@ -5,5 +5,5 @@
 //EVENT LISTENERS
 window.onload = init;
 takeTurn = document.getElementById("board").onclick
-init = document.getElementById("reset").onclick
+init = document.getElementById("reset-button").onclick
 //FUNCTIONS
